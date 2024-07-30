0ac44437dc92371570a2217d9587235548ba440f
Senthamizh-tech <in.ent.common.123@gmail.com>
2024-07-30 16:03:44 +0530

    new

---
+++ common/ResponseBuilder.js
@@ -0,0 +1,20 @@
+const xlsx = require("node-xlsx");
+
+module.exports = {
+    responseBuilder() {
+        const workbook = xlsx.parse(`./ESI_RESP_BUILDER_EN_CA_WEB.xlsx`);
+        const sheet = workbook[0].data;
+        const headers = sheet[0];
+        sampleJson = sheet.slice(1).map(row => {
+            let obj = {};
+            headers.forEach((header, index) => {
+                obj[header] = row[index] ? row[index] : null;
+            });
+            return obj;
+        });
+        console.log("workbook ===> ", sampleJson);
+
+        // const resultElement = sampleJson.filter((element) => element.RESPONSE_ID === 'ESI_CAN_PM_LIST_POKEMONS');
+        // console.log("resultElement ===> ", resultElement);
+    }
+}
